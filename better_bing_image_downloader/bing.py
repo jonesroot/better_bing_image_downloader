b71f2353933ccaeed0c65fd972158be78329ef08
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-09-11 04:53:27 +0700

    update better_bing_image_downloader/bing.py

--- better_bing_image_downloader/bing.py
+++ better_bing_image_downloader/bing.py
@@ -17,64 +17,7 @@ Improved Author: Krishnatejaswi S (shentharkrishnatejaswi@gmail.com)
 
 
 class Bing:
-    """_summary_
-    A class to download images from Bing.
-    
-    _description_
-    This class is used to download images from Bing. It uses the Bing Image Search API to get the links of the images and then downloads the images from the links. The class can be used to download images based on a query, with a limit on the number of images to be downloaded. The images can be filtered based on the type of image (photo, clipart, line drawing, animated gif, transparent) and the adult content can be filtered as well. The images are saved in the specified output directory. The class also has the option to be verbose, which will print the progress of the download.
-    
-    _parameters_
-    
-    query : str
-        The query to be used to search for images.
-    limit : int
-        The number of images to be downloaded.
-    output_dir : str
-        The directory where the images are to be saved.
-    adult : str
-        The adult content filter. Can be "off" or "on".
-    timeout : int
-        The time in seconds to wait for the request to Bing to be completed.
-    filter : str
-        The type of image to be filtered. Can be "line", "photo", "clipart", "gif", "transparent".
-    verbose : bool
-        Whether to print the progress of the download.
-        
-    _methods_
-    
-    get_filter(shorthand)
-        Returns the filter string based on the shorthand.
-        ============
-        shorthand : str
-            The shorthand for the filter. Can be "line", "photo", "clipart", "gif", "transparent".
-        ============
-        return : str
-            The filter string based on the shorthand.
-            
-    save_image(link, file_path)
-        Saves the image from the link to the file path.
-        ============
-        link : str
-            The link of the image to be saved.
-        file_path : str
-            The file path where the image is to be saved.
-        ============
-        return : None
-        
-    download_image(link)
-        Downloads the image from the link.
-        ============
-        link : str
-            The link of the image to be downloaded.
-        ============
-        return : None
-    run()
-        Runs the download of the images.
-        ============
-        return : None
-        
-    """
-    def __init__(self, query, limit, output_dir, adult, timeout, filter='', verbose=True, badsites=[], name='Image'):
+    def __init__(self, query, limit, output_dir, adult, timeout, filter='', verbose=False, badsites=[], name='Image'):
         self.download_count = 0
         self.query = query
         self.output_dir = output_dir
@@ -87,6 +30,11 @@ class Bing:
         self.image_name = name
         self.download_callback = None
         
+        if self.verbose:
+            logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+        else:
+            logging.basicConfig(level=logging.ERROR, format='%(levelname)s: %(message)s')
+        
         if self.badsites:
             logging.info("Download links will not include: %s", ', '.join(self.badsites))
 
@@ -97,13 +45,13 @@ class Bing:
 
         self.page_counter = 0
         self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
-    'AppleWebKit/537.11 (KHTML, like Gecko) '
-    'Chrome/23.0.1271.64 Safari/537.11',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    'Accept-Encoding': 'none',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Connection': 'keep-alive'}
+            'AppleWebKit/537.11 (KHTML, like Gecko) '
+            'Chrome/23.0.1271.64 Safari/537.11',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'}
 
     def get_filter(self, shorthand):
         if shorthand == "line" or shorthand == "linedrawing":
@@ -140,7 +88,6 @@ class Bing:
 
     def download_image(self, link):
         self.download_count += 1
-        # Get the image link
         try:
             path = urllib.parse.urlsplit(link).path
             filename = posixpath.basename(path).split('?')[0]
@@ -149,14 +96,14 @@ class Bing:
                 file_type = "jpg"
 
             if self.verbose:
-                print("[%] Downloading Image #{} from {}".format(self.download_count, link))
+                logging.info("[%] Downloading Image #{} from {}".format(self.download_count, link))
 
             self.save_image(link, self.output_dir.joinpath("{}_{}.{}".format(
                 self.image_name, str(self.download_count), file_type)))
+            
             if self.verbose:
-                print("[%] File Downloaded !\n")
+                logging.info("[%] File Downloaded !\n")
                 
-            # Update progress bar
             if self.download_callback:
                 self.download_callback(self.download_count)
 
@@ -167,8 +114,8 @@ class Bing:
     def run(self):
         while self.download_count < self.limit:
             if self.verbose:
-                logging.info('\n\n[!]Indexing page: %d\n', self.page_counter + 1)
-            # Parse the page source and download pics
+                logging.info('\n\n[!] Indexing page: %d\n', self.page_counter + 1)
+            
             try:
                 request_url = (
                     'https://www.bing.com/images/async?q='
@@ -190,14 +137,13 @@ class Bing:
                     logging.info("\n===============================================\n")
 
                 for link in links:
-
                     isbadsite = False
                     for badsite in self.badsites:
                         isbadsite = badsite in link
                         if isbadsite:
                             if self.verbose:
                                 logging.info("[!] Link included in badsites %s %s", badsite, link)
-                                break
+                            break
                     if isbadsite:
                         continue
 
@@ -207,7 +153,7 @@ class Bing:
 
                 self.page_counter += 1
             except urllib.error.HTTPError as e:
-                logging.error('URLError while making request to Bing: %s', e)
+                logging.error('HTTPError while making request to Bing: %s', e)
             except urllib.error.URLError as e:
                 logging.error('URLError while making request to Bing: %s', e)
 
